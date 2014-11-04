62a5bbade79159ee2e3448fccd6cfce18eff69ad
PalmPTSJ <palm_bubui@hotmail.com>
2014-11-04 22:20:59 +0700

    finishing debugphp work

--- php/upfile.php
+++ php/upfile.php
@@ -2,6 +2,13 @@
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["file"]["name"]);
 // $target_file is the path to store file (ex. uploads/myFile.txt)
-move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
+if(move_uploaded_file($_FILES["file"]["tmp_name"], $target_file))
+{
+	echo "File upload finished";
+}
+else
+{
+	echo "File upload failed ...";
+}
 // move the tmp_name file (temporary file) to the path ($target_file)
 ?>
